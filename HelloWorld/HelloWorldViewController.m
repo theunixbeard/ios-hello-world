587e97ddf7116715acc38df7781c670ed0537ed8
Ben Gelsey <ben.gelsey@gmail.com>
2013-02-22 16:39:33 -0800

    Hello World iPhone App

--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -10,20 +10,46 @@
 
 @interface HelloWorldViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+- (IBAction)changeGreeting:(id)sender;
+
 @end
 
 @implementation HelloWorldViewController
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)changeGreeting:(id)sender {
+  
+  self.userName = self.textField.text;
+  
+  NSString *nameString = self.userName;
+  if([nameString length] == 0) {
+    nameString = @"World";
+  }
+  NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+  self.label.text = greeting;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+  if (theTextField == self.textField) {
+    [theTextField resignFirstResponder];
+  }
+  return YES;
+}
+
 
 @end
